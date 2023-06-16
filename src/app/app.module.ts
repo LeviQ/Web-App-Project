0bdc25a618a7dd6b51db3e7feb9fbddee2f55aa3
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2023-06-16 12:14:07 +0200

    Code Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
